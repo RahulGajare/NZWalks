1fb830311d7c4e3d5e821b3b4491f37b5dd72aec
RahulGajare <rahulgajare0@gmail.com>
2026-05-17 17:09:23 +0530

    Added IdentityOptions

--- NZWalker.API/Data/NZWalksAuthDbContext.cs
+++ NZWalker.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalker.API.Data
@@ -8,6 +9,34 @@ namespace NZWalker.API.Data
 
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> dbContextOptions) : base(dbContextOptions)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = Guid.Parse("d1b9c8e7-5c3a-4f0e-9b2a-1a2b3c4d5e6f");
+            var writerRoleId = Guid.Parse("e2c8d9f8-6a4b-5f1e-0c3d-2b3c4d5e6f7a");
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId.ToString(),
+                    ConcurrencyStamp =readerRoleId.ToString(),
+                    Name = "reader",
+                    NormalizedName = "READER"
+                },
+                 new IdentityRole
+                {
+                    Id = writerRoleId.ToString(),
+                    ConcurrencyStamp =writerRoleId.ToString(),
+                    Name = "writer",
+                    NormalizedName = "Writer"
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
--- NZWalker.API/Program.cs
+++ NZWalker.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
@@ -28,6 +29,25 @@ builder.Services.AddScoped<IWalkRepository,SQLWalkRepository>();
 
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
+    .AddEntityFrameworkStores<NZWalksAuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars= 1;
+
+});
+
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options => 
